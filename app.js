0deb2e31858b6452cf9169411af860b6c3bda1c5
pandikapinata <pandikapinata27@gmail.com>
2018-07-02 19:56:58 +0800

    Listing Note

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -8,8 +6,7 @@ const notes = require('./notes.js');
 
 const argv = yargs.argv;
 var command = argv._[0];
-console.log('Command :',command);
-console.log('Yargs',argv);
+
 
 if (command === 'add'){
     var note = notes.addNote(argv.title, argv.body);
@@ -20,11 +17,11 @@ if (command === 'add'){
         console.log('Note title taken');
     }
 }else if(command === 'list'){
-    notes.getAll();
+    var allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s)`);
+    allNotes.forEach((note) => notes.logNote(note));
 }else if(command === 'read'){
     var readNote = notes.getNote(argv.title);
-    // console.log('====');
-    // console.log(readNote[0].title);
     if(readNote){
         notes.logNote(readNote);
     }else{
---
+++ playground/debugging.js
@@ -0,0 +1,7 @@
+var person = {
+    name: "Andrew"
+};
+
+person.age= 25;
+person.name='Mike';
+console.log(person);
